001e1db5892f841f32e819dcc0a4326588307c7c
GeertJohan <gjr19912@gmail.com>
2014-08-13 20:33:34 +0200

    Decoupled icon from box title. Add icon to posterless image. Improve events generation

--- js/timeline.js
+++ js/timeline.js
@@ -28,295 +28,345 @@ timeline.controller('MainCtrl', function ($scope, $sce) {
 	// className	string 			optional		one of the following classes: 'official', 'merchant', 'todo', 'make', 'more', 'classes'
 	var events = [
 		{
-			content: iconLaunch+'Launch',
+			icon: iconLaunch,
+			shortTitle: 'Launch',
 			start: '2014-04-04',
 			title: 'The return of the Gulden',
 			text: 'It\'s back!<br/>Stukje tekst over de lancering van Guldencoin. (TODO)',
 			image: 'http://www.ixperience.nl/wp-content/uploads/2014/05/Guldencoin.png',
 		},
 		{
-			content: iconExchange+'Added to Bittrex',
-			start: '2014-04-05',
+			icon: iconExchange,
+			shortTitle: 'Added to Bittrex',
 			title: 'Guldencoin got added to Bittrex',
+			start: '2014-04-05',
 		},
 		{
-			content: iconDeal+'Partnership with Litebit and Litepaid',
+			icon: iconDeal,
+			shortTitle: 'Partnership with Litebit and Litepaid',
 			start: '2014-04-08',
 			text: 'The guldencoin team works with Litebit and Litepaid.',
 		},
 		{
-			content: iconCommunity+'Guldencoinforum online',
+			icon: iconCommunity,
+			shortTitle: 'Guldencoinforum online',
 			start: '2014-04-11',
 			text: 'A well-known community member started a forum with english and dutch sections.',
 		},
 		{
-			content: iconOnlineMerchant+'biteweb.nl',
+			icon: iconOnlineMerchant,
+			shortTitle: 'biteweb.nl',
 			start: '2014-04-14',
 			text: 'A new online merchant started accepting Guldencoin: https://www.biteweb.nl/',
 		},
 		{
-			content: iconVideo+'Promotional video launched',
+			icon: iconVideo,
+			shortTitle: 'Promotional video launched',
 			start: '2014-04-18',
 			text: 'A video was made<br/>TODO: more information',
 			youtube: 'criatJSIqdE',
 		},
 		{
-			content: iconWalletUpdate+'New wallet OpenSSL Heartbleed fix',
+			icon: iconWalletUpdate,
+			shortTitle: 'New wallet OpenSSL Heartbleed fix',
 			start: '2014-04-20',
 			text: 'A new desktop wallet was released. Most importantly: OpenSSL Hearbleed fix.',
 		},
 		{
-			content: iconFood+'Subway Leeuwarden',
+			icon: iconFood,
+			shortTitle: 'Subway Leeuwarden',
 			start: '2014-04-25',
 			text: 'Subway Leeuwarden started accepting the Guldencoin.',
 			image: '/img/posters/payatsubwaynl.png',
 		},
 		{
-			content: iconMilestone+'Android wallet available',
+			icon: iconMilestone,
+			shortTitle: 'Android wallet available',
 			start: '2014-04-26',
 			text: 'The Android wallet was released.',
 		},
 		{
-			content: iconExchange+'Bleutrade exchange',
-			start: '2014-04-30',
+			icon: iconExchange,
+			shortTitle: 'Bleutrade exchange',
 			title: 'Added on Bluetrade Exchange',
+			start: '2014-04-30',
 			text: 'Guldencoin was added to <a href="https://bleutrade.com/">Bleutrade</a>. ',
 		},
 		{
-			content: iconFood+'Subway Assen',
-			start: '2014-05-02',
+			icon: iconFood,
+			shortTitle: 'Subway Assen',
 			title: 'Pay with NLG at Subway Assen',
+			start: '2014-05-02',
 			image: '/img/posters/payatsubwaynl.png',
 		},
 		{
-			content: iconOnlineMerchant+'New online merchants',
+			icon: iconOnlineMerchant,
+			shortTitle: 'New online merchants',
 			start: '2014-05-02',
 			text: 'http://www.speelkaartenmaken.nl/, http://www.memoryspelmaken.nl/, http://www.sportboekmaken.nl/'
 		},
 		{
-			content: iconFood+'Subway Kampen',
-			start: '2014-05-06',
+			icon: iconFood,
+			shortTitle: 'Subway Kampen',
 			title: 'Pay with NLG at Subway Kampen',
+			start: '2014-05-06',
 			image: '/img/posters/payatsubwaynl.png',
 		},
 		{
-			content: iconNewStuff+'Guldencoin marketplace guldenbeurs.nl',
+			icon: iconNewStuff,
+			shortTitle: 'Guldencoin marketplace guldenbeurs.nl',
 			start: '2014-05-09',
 			title: 'A guldencoin marketplace called <a href="http://guldenbeurs.nl/" >guldenbeurs.nl</a> opened it\'s doors online.',
 		},
 		{
-			content: iconOnlineMerchant+'Uncommon',
-			start: '2014-05-12',
+			icon: iconOnlineMerchant,
+			shortTitle: 'Uncommon',
 			title: 'New merchant Uncommon',
+			start: '2014-05-12',
 			text: 'Uncommon released a <a href="http://www.unco.nl/157/wij-accepteren-de-guldencoin/">blog post</a> stating they are now also accepting Guldencoin.'
 		},
 		{
-			content: iconFood+'De Prins, Amsterdam',
-			start: '2014-05-14',
+			icon: iconFood,
+			shortTitle: 'De Prins, Amsterdam',
 			title: 'Pay with Guldencoin video at De Prins',
+			start: '2014-05-14',
 			text: 'Guldencoin is accepted at \'De Prins\', a cafe located at the Amsterdam city center.',
 			image: '/img/posters/guldencoinatprins.png',
 		},
 		{
-			content: iconWalletUpdate+'New wallet',
+			icon: iconWalletUpdate,
+			shortTitle: 'New wallet',
 			start: '2014-05-16',
 		},
 		{
-			content: iconExchange+'Coinnext',
-			start: '2014-05-19',
+			icon: iconExchange,
+			shortTitle: 'Coinnext',
 			title: 'Added to coinnext exchange',
+			start: '2014-05-19',
 			text: 'Gudencoin has been <a href="https://coinnext.com/trade/NLG/BTC" >added</a> to the coinnext exchange.',
 		},
 		{
-			content: iconNewStuff+'betalenmetguldencoin.nl',
+			icon: iconNewStuff,
+			shortTitle: 'betalenmetguldencoin.nl',
 			start: '2014-05-20',
 			text: 'Easy way to find places were you can pay with nlg http://www.betalenmetguldencoin.nl/',
 		},
 		{
-			content: iconNewStuffOfficial+'docs.guldencoin.com released',
+			icon: iconNewStuffOfficial,
+			shortTitle: 'docs.guldencoin.com released',
 			start: '2014-05-26',
 			text: 'The <a href="https://docs.guldencoin.com" >docs.guldencoin.com</a> website contains (mostly technical) documentation about the coin and tools arround it.',
 		},
 		{
-			content: iconWalletUpdate+'Android Wallet v1.6',
-			start: '2014-06-02',
+			icon: iconWalletUpdate,
+			shortTitle: 'Android Wallet v1.6',
 			title: 'Version 1.6 of Android Wallet Released',
+			start: '2014-06-02',
 		},
 		{
-			content: iconNewStuff+'Guldencoin Wikipedia page online',
+			icon: iconNewStuff,
+			shortTitle: 'Guldencoin Wikipedia page online',
 			start: '2014-06-03',
 			text: 'The Guldencoin community created a <a href="http://nl.wikipedia.org/wiki/Guldencoin"> wikipedia page</a>',
 		},
 		{
-			content: iconExchange+'Swisscex',
-			start: '2014-06-04',
+			icon: iconExchange,
+			shortTitle: 'Swisscex',
 			title: 'Added to swisscex excange',
+			start: '2014-06-04',
 			text: 'The Guldencoin was added to the <a href="https://www.swisscex.com/market" >swisscex</a> exchange.'
 		},
 		{
-			content: iconGame+'World Cup 2014',
-			start: '2014-06-11',
+			icon: iconGame,
+			shortTitle: 'World Cup 2014',
 			title: 'Bet on World Cup 2014 matches with Guldencoin',
+			start: '2014-06-11',
 			text: 'A website called \'nlgcup.com\' was launched and allowed anyone to bet on the outcome of World Cup 2014 matches.',
 		},
 		{
-			content: iconNewStuff+'Community news website guldencoinweb.nl',
-			start: '2014-06-14',
+			icon: iconNewStuff,
+			shortTitle: 'Community news website guldencoinweb.nl',
 			title: 'Guldencoin Community News website released',
+			start: '2014-06-14',
 			text: '<a href="http://www.guldencoinweb.nl/">Guldencoinweb.nl</a> was launched. The website offers articles and information about the Guldencoin.',
 		},
 		{
-			content: iconNewStuff+'Guldencoin fauc.et',
-			start: '2014-06-17',
+			icon: iconNewStuff,
+			shortTitle: 'Guldencoin fauc.et',
 			title: 'New Guldencoin faucet was launched',
+			start: '2014-06-17',
 			text: '<a href="http://fauc.at/Index/Guldencoin">fauc.et</a> now provides free Guldencoin for anyone starting with Guldencoin',
 		},
 		{
-			content: iconNewStuff+'Added to whattomine.com',
-			start: '2014-06-19',
+			icon: iconNewStuff,
+			shortTitle: 'Added to whattomine.com',
 			title: 'Guldencoin added to whattomine.com',
+			start: '2014-06-19',
 			text: 'This site calculates which coins are profitable to mine',
 		},
 		{
-			content: iconNewStuff+'gratisguldencoin.nl',
-			start: '2014-06-20',
+			icon: iconNewStuff,
+			shortTitle: 'gratisguldencoin.nl',
 			title: 'A new site was launched: gratisguldencoin.nl',
+			start: '2014-06-20',
 			text: '<a href="http://gratisguldencoin.nl" >The site</a> is meant to be an advertisement and summary to explain to dutch citizen how to claim coins and info links',
 		},
 		{
-			content: iconDeal+'bitstraat.nl',
+			icon: iconDeal,
+			shortTitle: 'bitstraat.nl',
 			start: '2014-06-21',
 			text: '<a href="http://www.bitstraat.nl" >Bitstraat.nl</a> offers hardware solutions for merchants, which will make accepting Guldencoin easy and convenient.',
 			image: '/img/posters/guldencoinbitstraat.jpg',
 		},
 		{
-			content: iconOnlineMerchant+'bitscoinshop.com.br',
+			icon: iconOnlineMerchant,
+			shortTitle: 'bitscoinshop.com.br',
 			start: '2014-06-23',
 			text: '<a href="http://bitscoinshop.com.br/en/" >bitscoinshop.com.br</a> is the first international merchant to accept Guldencoin.',
 		},
 		{
-			content: iconNewStuffOfficial+'seeds.guldencoin.com',
+			icon: iconNewStuffOfficial,
+			shortTitle: 'seeds.guldencoin.com',
 			start: '2014-06-27',
 			text: '<a href="https://seeds.guldencoin.com/#/dashboard">seeds.guldencoin.com</a> provides information about the seeds and the network.',
 		},
 		{
-			content: iconMilestone+'New website guldencoin.com',
+			icon: iconMilestone,
+			shortTitle: 'New website guldencoin.com',
 			start: '2014-06-30',
 			text: 'The new <a href="https://guldencoin.com">guldencoin.com</a> website was launched.'
 		},
 		{
-			content: iconCommunity+'Eurocoins with Guldencoin stickers',
+			icon: iconCommunity,
+			shortTitle: 'Eurocoins with Guldencoin stickers',
 			start: '2014-07-03',
 			text: 'The Guldencoin community released the first eurocoins with Guldencoin advertisement stickers on them.',
 		},
 		{
-			content: iconNewStuffOfficial+'Guldensign webdeveloper tool released',
+			icon: iconNewStuffOfficial,
+			shortTitle: 'Guldensign webdeveloper tool released',
 			start: '2014-07-06',
 			text: 'The <a href="https://docs.guldencoin.com/guldensign/" >guldensign</a> was released. A html/css supplement that adds the Guldencoin sign to any website.',
 		},
 		{
-			content: iconNewStuffOfficial+'nlgcdn.com',
+			icon: iconNewStuffOfficial,
+			shortTitle: 'nlgcdn.com',
 			start: '2014-07-06',
 			text: 'The guldencoin cdn was released. Although not really a CDN at launch time, it\'s meant as a location where very commonly used resources are located to increase loading times of Guldencoin related websites. <a href="https://docs.guldencoin.com/services/cdn/">More info.</a>',
 		},
 		{
-			content: iconVideo+'Interview at \'De week van Bitcoin\'',
+			icon: iconVideo,
+			shortTitle: 'Interview at \'De week van Bitcoin\'',
 			start: '2014-07-10',
 			text: 'Interview by Paul Buitink and Wilbert Geers with Rijk Plasman about NLG.<br/><a href="http://debitcoin.org/de-week-van-bitcoin-35-de-terugkeer-van-de-gulden-met-efl-en-guldencoin/" >Original article at debitcoin.org.</a>',
 			youtube: '0hJYvUo8ghQ',
 		},
 		{
-			content: iconMilestone+'nlgCup.com closed after World Cup ended',
+			icon: iconMilestone,
+			shortTitle: 'nlgCup.com closed after World Cup ended',
 			start: '2014-07-13',
 			text: 'The <a href="http://nlgcup.com" >nlgcup site</a> ran a very successful betting game on the World Cup 2014. The site closed after the final payouts were made.',
 		},
 		{
-			content: iconNewStuffOfficial+'Guldenpay',
+			icon: iconNewStuffOfficial,
+			shortTitle: 'Guldenpay',
 			start: '2014-07-16',
 			text: 'Guldenpay is released along with Hardware(Optional) for making payments with Guldencoin exceptionally easy for offline merchants.',
 			image: '/img/posters/Guldenpay-webapp.jpg',
 		},
 		{
-			content: iconArticle+'Interview at AT5',
+			icon: iconArticle,
+			shortTitle: 'Interview at AT5',
 			start: '2014-07-20',
 			text: 'Interview with AT5 media about Guldenpay and cafetaria De Prins in Amsterdam using Guldenpay for NLG purchases.<br/> Listen to the interview on the <a href="http://www.at5.nl/artikelen/131523/patatje-sate-betalen-met-de-guldencoin" >AT5 website</a>.',
 		},
 		{
-			content: iconMilestone+'Guldencoin Website redesign',
+			icon: iconMilestone,
+			shortTitle: 'Guldencoin Website redesign',
 			start: '2014-07-24',
 			text: 'The Guldencoin website got major improvements again.',
 		},
 		{
-			content: iconArticle+'Interview with \'De Rijdende Reporter\'',
+			icon: iconArticle,
+			shortTitle: 'Interview with \'De Rijdende Reporter\'',
 			start: '2014-07-25',
 			text: 'Rijk Plasman did an interview with \'De Rijdende Reporter\'.<br/><a href="http://www.rtvnh.nl/programma-extra/12/0/De%20Rijdende%20Reporter" >Listen back here</a>.',
 		},
 		{
-			content: iconNewStuff+'New blockchain explorer guldentrader.nl',
+			icon: iconNewStuff,
+			shortTitle: 'New blockchain explorer guldentrader.nl',
 			start: '2014-07-28',
 			text: 'A new blockchain explorer was launched. <a href="http://www.guldencointrader.nl/" >guldencointrader.nl</a>',
 		},
 		{
-			content: iconArticle+'Article in \'DE ZAAK\'',
-			start: '2014-07-31',
+			icon: iconArticle,
+			shortTitle: 'Article in \'DE ZAAK\'',
 			title: 'Article in DE ZAAK',
+			start: '2014-07-31',
 			text: 'DE ZAAK is which a well know business site in the Netherlands. Read the article <a href="http://www.dezaak.nl/guldencoin-mobiel-betalen-met-virtuele-nl-munt-6798973.html" >here</a>.',
 		},
 		{
-			content: iconOfflineMerchant+'LaserCity in South-Holland now accepts Guldencoin.',
+			icon: iconOfflineMerchant,
+			shortTitle: 'LaserCity in South-Holland now accepts Guldencoin.',
 			start: '2014-08-04',
 			title: 'LaserCity in South-Holland now accepts Guldencoin.',
 			image: '/img/posters/guldencoinlasercity.jpg',
 		},
 		{
-			content: iconOnlineMerchant+'Online bookstore 1boek.nl',
-			start: '2014-08-06',
+			icon: iconOnlineMerchant,
+			shortTitle: 'Online bookstore 1boek.nl',
 			title: 'First online bookstore to accept NLG.',
+			start: '2014-08-06',
 			image: '/img/posters/guldencoin1boek.jpg',
 		},
 		{
-			content: iconOnlineMerchant+'Sliceweb',
+			icon: iconOnlineMerchant,
+			shortTitle: 'Sliceweb',
 			start: '2014-08-06',
 			text: 'Sliceweb is the first SEO company to accept Guldencoin.',
 			image: '/img/posters/guldencoinsliceweb.png',
 		},
 		{
-			content: iconMilestone+'Crowdfunding for iOS wallet started',
+			icon: iconMilestone,
+			shortTitle: 'Crowdfunding for iOS wallet started',
 			start: '2014-08-07',
 		},
 		{
-			content: iconMilestone+"Crowdfunding reached 70% within the first 12 hours",
+			icon: iconMilestone,
+			shortTitle: "Crowdfunding reached 70% within the first 12 hours",
 			start: '2014-08-08',
 		},
 		{
-			content: iconOnlineMerchant+'BalkonBBQ.com',
+			icon: iconOnlineMerchant,
+			shortTitle: 'BalkonBBQ.com',
 			start: '2014-08-09', // actually the 8th, but moved to make everything readable..
 			image: '/img/posters/guldencoinbalkonbbq.jpg',
 		},
 		{
-			content: iconOnlineMerchant+'nerf-kopen.com, LittlePeopleSpeelgoed.com and more',
+			icon: iconOnlineMerchant,
+			shortTitle: 'nerf-kopen.com, LittlePeopleSpeelgoed.com and more',
 			start: '2014-08-09', // actually the 8th, but moved to make everything readable..
 			text: 'A group of websites started accepting Guldencoin:<br/><a href="http://www.Alarmpistoolkopen.nl" >Alarmpistoolkopen.nl</a><br/><a href="http://www.Nerf-kopen.com" >Nerf-kopen.com</a><br/><a href="http://www.Spiongadgets.nl" >Spiongadgets.nl</a><br/><a href="http://www.LittlePeopleSpeelgoed.com" >LittlePeopleSpeelgoed.com</a>',
 			image: '/img/posters/littlepeoplespeelgoed.jpg',
 		},
 		{
-			content: iconMilestone+'Guldencoin Timeline (this site) launched!',
+			icon: iconMilestone,
+			shortTitle: 'Guldencoin Timeline (this site) launched!',
 			start: '2014-08-14',
 			text: 'This website was launched. Did you know it was launched on the creators\' birthday? Now let\'s have some cake!',
 		}
 	];
 
 	// fix id's on events
-	var i = 1;
-	events.forEach(function(ev) {
-		ev.id = i;
-		i++;
+	events.forEach(function(ev, i) {
+		// set id
+		ev.id = i+1;
+
+		// set item field content (displayed on timeline)
+		ev.content = ev.icon + ev.shortTitle;
 	});
 
 	// TODO:
-	// text onder image bij kleine schermen
-	// icoon los koppelen in losse variabele, dan icoon met groot fontsize gebruiken in image als er geen specifieke image beschikbaar is
-	// ook content in losse param zetten: 'subject' of 'shortTitle'. content zelf compilen door icoon+subject. title (indien unset) invullen met subject ZONDER icoon.
 	// Date formatting (2014 April 1) ipv (Apr 1, 2014)
 		
 
@@ -325,19 +375,20 @@ timeline.controller('MainCtrl', function ($scope, $sce) {
 		console.dir(ev);
 		angular.copy(ev, $scope.event);
 
-		// default image when there is no image
-		if(angular.isUndefined($scope.event.image) && angular.isUndefined($scope.event.youtube)) {
-			$scope.event.image = 'https://guldencoin.com/wp-content/themes/gulden/images/cover.png';
-		}
+		// // default image when there is no image
+		// if(angular.isUndefined($scope.event.image) && angular.isUndefined($scope.event.youtube)) {
+		// 	$scope.event.image = '';
+		// }
 
 		// set title if it's not set (use content)
 		if(angular.isUndefined($scope.event.title)) {
-			$scope.event.title = $scope.event.content;
+			$scope.event.title = $scope.event.shortTitle;
 		}
 
 		// sce clearings
 		$scope.event.title = $sce.trustAsHtml($scope.event.title);
 		$scope.event.text = $sce.trustAsHtml($scope.event.text);
+		$scope.event.icon = $sce.trustAsHtml($scope.event.icon);
 	}
 	$scope.updateEvent(events[0]);
 
